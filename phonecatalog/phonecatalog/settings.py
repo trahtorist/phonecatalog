38bae72847542ce8cc899b2ed20c6201cd62b7df
trahtorist <rtbtrt5@gmail.com>
2022-09-11 14:50:46 +0500

    add - phonecatalog/phonecatalog/settings.py

--- phonecatalog/phonecatalog/settings.py
+++ phonecatalog/phonecatalog/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -77,6 +77,13 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ["POSTGRES_DB"],
+        'USER': os.environ["POSTGRES_USER"],
+        'PASSWORD': os.environ["POSTGRES_PASSWORD"],
+        'HOST': os.environ["DATABASE_HOST"],  # <-- IMPORTANT: same name as docker-compose service!
+        'PORT': os.environ["DATABASE_PORT"],
+
     }
 }
 
