4e41363887be4545537b7b08689bb18122a6dbb6
trahtorist <rtbtrt5@gmail.com>
2022-09-12 14:56:40 +0500

    modified: phonecatalog/gbwphone/models.py- add models  modified: phonecatalog/phonecatalog/settings.py - fix added: phonecatalog/gbwphone/migrations/0001_initial.py

--- phonecatalog/gbwphone/models.py
+++ phonecatalog/gbwphone/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class PhoneCatalog(models.Model):
+    manufacturer = models.CharField(max_length=255)
+    device_name = models.CharField(max_length=255)
+    device_model = models.CharField(max_length=255)
+    active = models.BooleanField()
--- phonecatalog/phonecatalog/settings.py
+++ phonecatalog/phonecatalog/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'gbwphone',
+
 ]
 
 MIDDLEWARE = [
@@ -75,10 +77,10 @@ WSGI_APPLICATION = 'phonecatalog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+
+       
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.environ["POSTGRES_DB"],
+       'NAME': os.environ["POSTGRES_DB"],
         'USER': os.environ["POSTGRES_USER"],
         'PASSWORD': os.environ["POSTGRES_PASSWORD"],
         'HOST': os.environ["DATABASE_HOST"],  # <-- IMPORTANT: same name as docker-compose service!
