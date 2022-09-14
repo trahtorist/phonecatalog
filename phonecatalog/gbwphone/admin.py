2eed252ad13d25b339ad155a7582e076e2645fdf
trahtorist <rtbtrt5@gmail.com>
2022-09-14 15:51:11 +0500

    active default

--- phonecatalog/gbwphone/admin.py
+++ phonecatalog/gbwphone/admin.py
@@ -2,9 +2,10 @@ from django.contrib import admin
 from gbwphone.models import PhoneCatalog
 
 
-class gbwphoneAdmin(admin.ModelAdmin):
-    list_dispay = ("id", "manufacturer", "device_name", "device_model", "active")
-    search_fields = ("manufacturer","device_name")
-    list_filter = ("manufacturer","device_name")
+class GbwphoneAdmin(admin.ModelAdmin):
+    list_display = ("id", "manufacturer", "device_name", "device_model", "active")
+    search_fields = ("manufacturer", "device_name")
+    list_filter = ("manufacturer", "device_name")
 
-admin.site.register(PhoneCatalog, gbwphoneAdmin)
+
+admin.site.register(PhoneCatalog, GbwphoneAdmin)
--- phonecatalog/gbwphone/models.py
+++ phonecatalog/gbwphone/models.py
@@ -5,4 +5,4 @@ class PhoneCatalog(models.Model):
     manufacturer = models.CharField(max_length=255)
     device_name = models.CharField(max_length=255)
     device_model = models.CharField(max_length=255)
-    active = models.BooleanField()
+    active = models.BooleanField(default=True)
