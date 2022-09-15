fb2e5f8df9221c28590dabf496381492cd003949
trahtorist <rtbtrt5@gmail.com>
2022-09-15 12:29:02 +0500

    fix name class

--- phonecatalog/gbwphone/admin.py
+++ phonecatalog/gbwphone/admin.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
 from gbwphone.models import PhoneCatalog
-from .models import *
 
-class GbwphoneAdmin(admin.ModelAdmin):
+
+class PhoneCatalogAdmin(admin.ModelAdmin):
     list_display = ("id", "manufacturer", "device_name", "device_model", "active")
     search_fields = ("manufacturer", "device_name")
     list_filter = ("manufacturer", "device_name", "active")
 
 
-admin.site.register(PhoneCatalog, GbwphoneAdmin)
+admin.site.register(PhoneCatalog,PhoneCatalogAdmin)
