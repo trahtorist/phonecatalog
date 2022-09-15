8b7712da794a7db544b813ef703fe15786a35489
trahtorist <rtbtrt5@gmail.com>
2022-09-15 11:17:46 +0500

    add new taxfree admin

--- phonecatalog/taxfree/admin.py
+++ phonecatalog/taxfree/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from taxfree.models import Puesc
+from .models import *
 
-# Register your models here.
+class TaxfreeAdmin(admin.ModelAdmin):
+    list_display = ("taxid","puesc_answer")
+    search_fields = ("taxid", "puesc_answer")
+    list_filter = ("taxid", "puesc_answer")
+
+admin.site.register(Puesc, TaxfreeAdmin)
--- phonecatalog/taxfree/models.py
+++ phonecatalog/taxfree/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+
+class Puesc(models.Model):
+    taxid = models.CharField(max_length=255)
+    puesc_answer = models.TextField(blank=True)
