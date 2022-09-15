3ed38e2b526001a41b442f63128efebed9de82ce
trahtorist <rtbtrt5@gmail.com>
2022-09-15 11:46:53 +0500

    fix search

--- phonecatalog/taxfree/admin.py
+++ phonecatalog/taxfree/admin.py
@@ -2,8 +2,9 @@ from django.contrib import admin
 from taxfree.models import Puesc
 from .models import *
 
+
 class PuescAdmin(admin.ModelAdmin):
-    list_display = ("taxid","puesc_answer")
-    search_fields = ("taxid", "puesc_answer")
+    list_display = ("taxid", "puesc_answer")
+
 
 admin.site.register(Puesc, PuescAdmin)
