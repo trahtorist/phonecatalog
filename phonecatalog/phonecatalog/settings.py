40e9a68692b2fdb5db2ab5a65bfd2c0e095023bc
trahtorist <rtbtrt5@gmail.com>
2022-09-15 09:14:17 +0500

    add new taxfree application

--- phonecatalog/phonecatalog/settings.py
+++ phonecatalog/phonecatalog/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'gbwphone',
-]
+    ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
